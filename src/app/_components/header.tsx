bbc1fd6ae808e562a2483721b20617153ecd0f40
Gabriel Brum da Luz <biel.brumluz@gmail.com>
2024-08-08 20:15:48 -0300

    feat: add link to header logo

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -4,12 +4,15 @@ import { Card, CardContent } from "./ui/card"
 import Image from "next/image"
 import { Sheet, SheetTrigger } from "./ui/sheet"
 import SidebarSheet from "./sidebar-sheet"
+import Link from "next/link"
 
 export function Header() {
   return (
     <Card className="rounded-t-none">
       <CardContent className="flex flex-row items-center justify-between p-5">
-        <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
+        <Link href="/">
+          <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
+        </Link>
 
         <Sheet>
           <SheetTrigger asChild>
