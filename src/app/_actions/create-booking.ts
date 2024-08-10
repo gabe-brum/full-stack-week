d141af8e597e7308abfe2f2208f3794dc3e0d48b
Gabriel Brum da Luz <biel.brumluz@gmail.com>
2024-08-09 23:11:52 -0300

    feat: ensure only authenticated user can book

--- src/app/_actions/create-booking.ts
+++ src/app/_actions/create-booking.ts
@@ -2,16 +2,23 @@
 
 import { revalidatePath } from "next/cache"
 import { db } from "../_lib/prisma"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../_lib/auth"
 
 interface CreateBookingProps {
-  userId: string
   serviceId: string
   date: Date
 }
 
 export const createBooking = async (params: CreateBookingProps) => {
+  const userSession = await getServerSession(authOptions)
+
+  if (!userSession) {
+    throw new Error("Usuário não autenticado")
+  }
+
   await db.booking.create({
-    data: params,
+    data: { ...params, userId: (userSession.user as any).id },
   })
   revalidatePath("/barbershop/[id]")
 }
